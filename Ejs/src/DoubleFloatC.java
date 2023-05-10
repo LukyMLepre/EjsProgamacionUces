c359d2d6df28b0463f841894270b3507258ae525
Lukymlepre <lucas.lepre@comunidad.uces.edu.ar>
2023-05-09 21:57:01 -0300

    primer commit

---
+++ Ejs/src/DoubleFloatC.java
@@ -0,0 +1,7 @@
+public class DoubleFloatC {
+    public static void main(String[] args) {
+        double num = 2.2;
+        System.out.printf("Numero: %f \n",num);
+    }
+
+    }
---
+++ Ejs/src/OpNull.java
@@ -0,0 +1,16 @@
+public class OpNull {
+
+    public static void main(String args[]){
+        promptUser();
+    }
+
+    private static void promptUser(){
+    String name = "pablo";
+    if(name == null || name.trim().isEmpty()){
+        System.out.println("No entro su nombre");
+
+    }else {
+        System.out.println("Entro su nombre: " + name);
+    }
+    }
+}
